0e4bc5ad6ba0ee37b83485004f78d1751230c494
jaehak <dma963@naver.com>
2021-02-22 23:08:35 +0900

    비구조화 할당 문법을 통한 props 값 추출

--- cheapter3/src/MyComponent.js
+++ cheapter3/src/MyComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const MyComponent = props => {
-    return  <div> 안녕하세요. 제 이름은 {props.name}입니다. <br />
-        Children 값은 {props.children} 입니다.
+const MyComponent = ({ name, children }) => {
+    return  <div> 안녕하세요. 제 이름은 {name}입니다. <br />
+        Children 값은 {children} 입니다.
      </div>;
 };
 
