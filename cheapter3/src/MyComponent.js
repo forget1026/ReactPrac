74891f08aef554f84c0a3dd2521060bfbd671020
jaehak <dma963@naver.com>
2021-02-22 23:12:05 +0900

    PropTypes 사용

--- cheapter3/src/MyComponent.js
+++ cheapter3/src/MyComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MyComponent = ({ name, children }) => {
     return  <div> 안녕하세요. 제 이름은 {name}입니다. <br />
@@ -9,4 +10,7 @@ const MyComponent = ({ name, children }) => {
 MyComponent.defaultProps = {
     name: '기본 이름'
 };
+MyComponent.propTypes = {
+    name: PropTypes.string
+}; // 이렇게 설정하면 props를 전달 할 때 name에 string값을 전달해야만 한다. 다만 경고메시지 자체는 console에 표시된다.
 export default MyComponent;
