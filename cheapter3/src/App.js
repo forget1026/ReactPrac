da7e1d994b25d55a48e51cb3515d581d5610afff
jaehak <dma963@naver.com>
2021-02-22 23:51:50 +0900

    use State 사용하기

--- cheapter3/src/App.js
+++ cheapter3/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Counter from './Counter';
+import Say from './Say';
 
 
 const App = () => {
   return <>
       <Counter></Counter>
+      <Say></Say>
     </>;
 }
 export default App;
--- cheapter3/src/Counter.js
+++ cheapter3/src/Counter.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);        
-        this.state = { // state 설정시 constructor에 작성하여 설정
-            number: 0,
-            fixedNumber: 0
-        };
-    }
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
 
     render() {
         const { number, fixedNumber } = this.state; // state의 값 조회시 this.state로 조회할 수 있음
@@ -16,7 +13,7 @@ class Counter extends Component {
                 <h1>{number}</h1>
                 <h2>고정 값 : {fixedNumber}</h2>
                 <button onClick={() => {
-                    this.setState({number: number + 1}); // this.setState를 통해 state의 값 변경가능
+                    this.setState({number: number + 1}, () => {console.log(this.state)}); // this.setState를 통해 state의 값 변경가능. callback함수를 통한 state값 확인
                 }}>
                     +1
                 </button>
